9c264d58edf108332bd86ba07930ea2ae25597c5
Luis Ramirez <luis@hall-inc.com>
2014-05-28 15:29:06 -0700

    derp

--- MWPhotoBrowser/Classes/MWPhotoBrowser.m
+++ MWPhotoBrowser/Classes/MWPhotoBrowser.m
@@ -1084,7 +1084,7 @@
         if ([_delegate respondsToSelector:@selector(photoBrowser:titleForPhotoAtIndex:)]) {
             self.title = [_delegate photoBrowser:self titleForPhotoAtIndex:_currentPageIndex];
         } else {
-            self.title = [NSString stringWithFormat:@"%lu %@ %lu", (unsigned long)(numberOfPhotos - _currentPageIndex), NSLocalizedString(@"of", @"Used in the context: 'Showing 1 of 3 items'"), (unsigned long)numberOfPhotos];
+            self.title = [NSString stringWithFormat:@"%lu %@ %lu", (unsigned long)(_currentPageIndex+1), NSLocalizedString(@"of", @"Used in the context: 'Showing 1 of 3 items'"), (unsigned long)numberOfPhotos];
         }
 	} else {
 		self.title = nil;
